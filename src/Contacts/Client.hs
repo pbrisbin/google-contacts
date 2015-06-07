c314436a779f560206196212c20e7a9d79813dd8
patrick brisbin <pbrisbin@gmail.com>
2015-06-07 13:40:30 -0400

    getFeedJSON returns FromJSON a

--- src/Contacts/Client.hs
+++ src/Contacts/Client.hs
@@ -2,7 +2,7 @@ module Contacts.Client
     ( getFeedJSON
     ) where
 
-import Data.ByteString.Lazy (ByteString)
+import Data.Aeson (FromJSON(..), eitherDecode)
 import Data.Monoid ((<>))
 import Network.Google.OAuth2 (OAuth2Token)
 import Network.HTTP.Conduit
@@ -16,12 +16,12 @@ import Network.HTTP.Types (hAuthorization)
 
 import qualified Data.ByteString.Char8 as B8
 
-getFeedJSON :: OAuth2Token -> String -> IO ByteString
+getFeedJSON :: FromJSON a => OAuth2Token -> String -> IO (Either String a)
 getFeedJSON token email = do
     request <- parseUrl feedUrl
     response <- withManager $ httpLbs $ authorize request
 
-    return $ responseBody response
+    return $ eitherDecode $ responseBody response
 
  where
     feedUrl = "https://www.google.com/m8/feeds/contacts/" <> email <> "/full?alt=json"
