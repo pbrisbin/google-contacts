453619e469f68d903f393732e1d52ee910edacac
patrick brisbin <pbrisbin@gmail.com>
2018-04-24 12:00:48 -0400

    Modernize

--- src/Google/Contacts/Client.hs
+++ src/Google/Contacts/Client.hs
@@ -1,32 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Google.Contacts.Client
     ( getFeedJSON
     ) where
 
-import Data.Aeson (FromJSON(..), eitherDecode)
+import Control.Exception.Safe (handleAny)
+import Data.Aeson
 import Data.Monoid ((<>))
 import Network.Google.OAuth2 (OAuth2Token)
-import Network.HTTP.Conduit
-    ( httpLbs
-    , parseUrl
-    , requestHeaders
-    , responseBody
-    , withManager
-    )
-import Network.HTTP.Types (hAuthorization)
+import Network.HTTP.Simple
 
 import qualified Data.ByteString.Char8 as B8
 
 getFeedJSON :: FromJSON a => OAuth2Token -> String -> IO (Either String a)
-getFeedJSON token email = do
-    request <- parseUrl feedUrl
-    response <- withManager $ httpLbs $ authorize request
+getFeedJSON token email = handleAny (pure . Left . show) $ do
+    request <- parseRequest
+        $ "https://www.google.com/m8/feeds/contacts/"
+        <> email <> "/full?max-results=1000&alt=json"
 
-    return $ eitherDecode $ responseBody response
-
- where
-    feedUrl = "https://www.google.com/m8/feeds/contacts/"
-        <> email
-        <> "/full?max-results=1000&alt=json"
-
-    authorize request = request
-        { requestHeaders = [(hAuthorization, B8.pack $ "Bearer " <> token)] }
+    Right . getResponseBody <$> httpJSON (authorize request)
+  where
+    authorize = setRequestHeaders
+        [ ("Authorization", B8.pack $ "Bearer " <> token)
+        ]
